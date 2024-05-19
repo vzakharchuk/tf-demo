c1b3eb857a6d8eeb0bf3364327100c8e600db44e
Vadym <zakharchuk_vadym@gmail.com>
2024-05-19 19:01:23 +0000

    update

--- tf-google-gke-cluster/main.tf
+++ tf-google-gke-cluster/main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "github.com/<ВАШ-РЕПОЗИТОРІЙ>/tf-google-gke-cluster"
+  source         = "github.com/vzakharchuk/tf-demo/tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
--- tf/vars.tfvars
+++
@@ -1,4 +0,0 @@
-GOOGLE_REGION = "us-central1"
-GOOGLE_PROJECT = "my-google-project"
-GKE_NUM_NODES = 3
-
