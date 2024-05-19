4db83657c88737cd8046516292aa7921fb389696
Vadym <zakharchuk_vadym@gmail.com>
2024-05-19 19:42:26 +0000

    update again

--- tf-google-gke-cluster/main.tf
+++ tf-google-gke-cluster/main.tf
@@ -1,5 +1,5 @@
 module "gke_cluster" {
-  source         = "github.com/vzakharchuk/tf-demo"
+  source         = "github.com/vzakharchuk/tf-demo//tf-google-gke-cluster"
   GOOGLE_REGION  = var.GOOGLE_REGION
   GOOGLE_PROJECT = var.GOOGLE_PROJECT
   GKE_NUM_NODES  = 2
